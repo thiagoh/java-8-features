1d8fccf72d43425919e866f780c1bd34557a0217
Thiago Andrade <thiagoh@gmail.com>
2015-10-14 00:04:05 -0300

    script writting files

--- src/com/thiagoh/java8/nashorn/NashornTest2.java
+++ src/com/thiagoh/java8/nashorn/NashornTest2.java
@@ -1,8 +1,13 @@
 package com.thiagoh.java8.nashorn;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
@@ -16,13 +21,41 @@ import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
 public class NashornTest2 {
 
+	private static String _PATH = "com/thiagoh/java8/nashorn/value.js";
+
 	public static void main(String[] args) {
 
 		ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
 
 		int c = 0;
+
 		while (c++ < 10) {
+
 			load(contextClassLoader);
+
+			try {
+
+				Thread.sleep(1000);
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			URL resourceUrl = contextClassLoader.getResource(_PATH);
+
+			try {
+
+				File file = new File(resourceUrl.toURI());
+
+				try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");) {
+
+					randomAccessFile.seek(0);
+					randomAccessFile.writeUTF("var value = " + c + ";");
+				}
+
+			} catch (URISyntaxException | IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -34,7 +67,7 @@ public class NashornTest2 {
 
 		try {
 
-			URL resourceUrl = contextClassLoader.getResource("/com/thiagoh/java8/nashorn/charts.js");
+			URL resourceUrl = contextClassLoader.getResource(_PATH);
 
 			URLConnection urlConnection = resourceUrl.openConnection();
 
@@ -48,48 +81,11 @@ public class NashornTest2 {
 			e.printStackTrace();
 		}
 
-		Invocable invocable = (Invocable) engine;
+		long t1 = System.currentTimeMillis();
+		Object result = engine.get("value");
+		long t2 = System.currentTimeMillis();
 
-		try {
-
-			long t1 = System.currentTimeMillis();
-			Object result = invocable.invokeFunction("getChart");
-
-			ScriptObjectMirror mirror = (ScriptObjectMirror) result;
-
-			long t2 = System.currentTimeMillis();
-
-			System.out.println(result.toString() + " in " + (t2 - t1) + "ms");
-
-			System.out.println(Arrays.asList(mirror.getOwnKeys(true)).stream().reduce("", (acc, key) -> {
-
-				return acc + key + ": " + mirror.get(key) + ",\n";
-
-			}) + " in " + (t2 - t1) + "ms");
-
-		} catch (NoSuchMethodException | ScriptException e) {
-			e.printStackTrace();
-		}
-
+		System.out.println(result.toString() + " in " + (t2 - t1) + "ms");
 	}
 
-	private static boolean getStatus(Entidade entidade) {
-
-		Entidade child = EntidadeUtil.getAcaoPrioritariaList().stream().filter((cur) -> {
-
-			// System.out.println("previsto: " + cur.getPrevisto() +
-			// " realizado: " + cur.getRealizado());
-
-				return cur.getRealizado() < cur.getPrevisto() * 0.01;
-
-			}).findFirst().get();
-
-		System.out.println("Result child: " + child);
-
-		if (child == null) {
-			return false;
-		}
-
-		return true;
-	}
 }
